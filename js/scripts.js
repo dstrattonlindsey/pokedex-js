7e684593aca6d9b6626711f41511df7350db0a49
dstrattonlindsey <danni.lindsey@outlook.com>
2021-08-03 16:36:57 +0200

    added new variable with array {name. height, type)
    added new variable (pkemonList) with array {name. height, type) for pokemon 1-9.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,13 @@
-alert('Hello world');
+let pokemonList = [
 
- let favoriteFood = 'pasta';
- document.write(favoriteFood);
+  { name: 'Bulbasaur', height: 2, types: ['Grass', 'Poison'] },
+  { name: 'Ivysaur', height: 3, types: ['Grass', 'Poison'] },
+  { name: 'Venusaur', height: 6, types: ['Grass', 'Poison'] },
+  { name: 'Charmander', height: 2, types: ['Fire'] },
+  { name: 'Charmeleon', height: 3, types: ['Fire'] },
+  { name: 'Charizard', height: 5, types: ['Fire', 'Flying'] },
+  { name: 'Squirtle', height: 1, types: ['Water'] },
+  { name: 'Wartortle', height: 3, types: ['Water'] },
+  { name: 'Blastoise', height: 5, types: ['Water'] },
+
+];
