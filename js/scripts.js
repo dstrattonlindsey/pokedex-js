64a78f26a8b5958313e85648fb76ef6f8a7cd269
dstrattonlindsey <danni.lindsey@outlook.com>
2021-08-14 18:06:51 +0200

    organized array and added pokemon name and height to DOM in script.js
    organized array to make it more easily readable. Added pokemon name and height. Display "Wow! That's big!" for pokemon height more than 5 to DOM.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,57 @@
 let pokemonList = [
 
-  { name: 'Bulbasaur', height: 2, types: ['Grass', 'Poison'] },
-  { name: 'Ivysaur', height: 3, types: ['Grass', 'Poison'] },
-  { name: 'Venusaur', height: 6, types: ['Grass', 'Poison'] },
-  { name: 'Charmander', height: 2, types: ['Fire'] },
-  { name: 'Charmeleon', height: 3, types: ['Fire'] },
-  { name: 'Charizard', height: 5, types: ['Fire', 'Flying'] },
-  { name: 'Squirtle', height: 1, types: ['Water'] },
-  { name: 'Wartortle', height: 3, types: ['Water'] },
-  { name: 'Blastoise', height: 5, types: ['Water'] },
+  { name: 'Bulbasaur',
+    height: 2.04,
+    types: ['Grass', 'Poison']
+  },
+
+  { name: 'Ivysaur',
+    height: 3.03,
+    types: ['Grass', 'Poison']
+  },
+
+  { name: 'Venusaur',
+    height: 6.07,
+    types: ['Grass', 'Poison']
+  },
+
+  { name: 'Charmander',
+    height: 2.00,
+    types: ['Fire']
+  },
+
+  { name: 'Charmeleon',
+    height: 3.07,
+    types: ['Fire']
+  },
+
+  { name: 'Charizard',
+    height: 5.07,
+    types: ['Fire', 'Flying']
+  },
+
+  { name: 'Squirtle',
+    height: 1.08,
+    types: ['Water']
+  },
+
+  { name: 'Wartortle',
+    height: 3.03,
+    types: ['Water']
+  },
+
+  { name: 'Blastoise',
+    height: 5.03,
+    types: ['Water']
+  },
 
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+  document.write (pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')');
+
+  if (pokemonList[i].height > 5) {
+    document.write(" Wow! that's big!");
+  }
+   document.write ('</br>')
+  }
